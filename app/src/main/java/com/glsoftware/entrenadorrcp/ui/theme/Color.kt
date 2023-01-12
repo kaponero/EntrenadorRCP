0ee9bee2a3aaebe471e36751c88adb3142a49223
kaponero <105125713+kaponero@users.noreply.github.com>
2023-01-12 18:03:33 -0300

    agregando thema

--- app/src/main/java/com/glsoftware/entrenadorrcp/ui/theme/Color.kt
+++ app/src/main/java/com/glsoftware/entrenadorrcp/ui/theme/Color.kt
@@ -2,7 +2,28 @@ package com.glsoftware.entrenadorrcp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Red200 = Color(0xFFED5F7D)
+val Red500 = Color(0xFFD13751)
+val Red700 = Color(0xFFC1182E)
 val Teal200 = Color(0xFF03DAC5)
+val Grey = Color(0xAAAAAAAA)
+val Bordo = Color(0xCCC1182E)
+val DarkBordo = Color(0x55C1182E)
+
+val black = Color(0xFF000000)
+val darkGray = Color(0xFF1a1717)
+val darkBlue = Color(0xFF18192b)
+val white = Color(0xFFF3F3F3)
+val gray = Color(0xFF3F3F3F)
+val blueGray = Color(0xFF404352)
+val nightDark = Color(0xFF403757)
+
+val purple = Color(0xFF9b11ba)
+val orange = Color(0xFFdb660d)
+val redOrange = Color(0xFFe84a23)
+val green = Color(0xFF0ddb25)
+val blue = Color(0xFF140ddb)
+val brightBlue = Color(0xFF027cf5)
+
+val ligtgreen = Color(0xFF0dFF25)
+val hand_green = Color(0xFF40E834)
